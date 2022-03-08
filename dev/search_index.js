b16c82ee9271352853bd875fc8e89e2f2855d58a
Documenter.jl <documenter@juliadocs.github.io>
2022-03-08 12:28:38 +0000

    build based on f8ab9a4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StorageOrders","category":"page"},{"location":"#StorageOrder","page":"Home","title":"StorageOrder","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StorageOrders.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StorageOrders]","category":"page"}]
+}
