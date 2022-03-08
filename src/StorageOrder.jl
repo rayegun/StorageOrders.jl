9a0a7ea699b6452b62b07937eeefbacce78b22ba
Wimmerer <kimmerer@mit.edu>
2022-03-08 06:37:45 -0500

    init

--- src/StorageOrder.jl
+++ src/StorageOrder.jl
@@ -1,5 +1,9 @@
 module StorageOrder
 
-# Write your package code here.
+abstract type StorageOrder end
+struct ColMajor <: StorageOrder end #colexicographic ordering
+struct RowMajor <: StorageOrder end #lexicographic ordering
+struct NoOrder <: StorageOrder end
 
+storageorder(::AbstractArray) = ColMajor() # fallback to the the Julia default.
 end
