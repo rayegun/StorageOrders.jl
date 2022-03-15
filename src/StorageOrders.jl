11320dded46fd1d9440b1e9894ccf01aa9d9050c
Wimmerer <kimmerer@mit.edu>
2022-03-15 09:03:05 -0400

    export and add RuntimeOrder

--- src/StorageOrders.jl
+++ src/StorageOrders.jl
@@ -1,9 +1,11 @@
 module StorageOrders
 
+export storageorder, ColMajor, RowMajor, NoOrder, RuntimeOrder
 abstract type StorageOrder end
 struct ColMajor <: StorageOrder end #colexicographic ordering
 struct RowMajor <: StorageOrder end #lexicographic ordering
 struct NoOrder <: StorageOrder end # something like a hash map sparse array.
+struct RuntimeOrder <: StorageOrder end # Order is known only at runtime, needs function barrier or manual redispatch.
 
 storageorder(::AbstractArray) = ColMajor() # fallback to the the Julia default.
 
