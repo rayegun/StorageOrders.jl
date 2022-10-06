74d6a54bee7506899b3e71e1b058a1ff0a7a8466
Will Kimmerer <kimmerer@mit.edu>
2022-10-06 09:16:42 -0400

    add runtime order support, untested

--- src/StorageOrders.jl
+++ src/StorageOrders.jl
@@ -7,6 +7,11 @@ struct RowMajor <: StorageOrder end #lexicographic ordering
 struct NoOrder <: StorageOrder end # something like a hash map sparse array.
 struct RuntimeOrder <: StorageOrder end # Order is known only at runtime, needs function barrier or manual redispatch.
 
-storageorder(::AbstractArray) = ColMajor() # fallback to the the Julia default.
+function storageorder(A::AbstractArray)
+    order = comptime_storageorder(A)
+    return order === RuntimeOrder ? runtime_storageorder(A) : order
+end
 
+comptime_storageorder(::AbstractArray) = ColMajor()
+runtime_storageorder(::AbstractArray) = ColMajor()
 end
