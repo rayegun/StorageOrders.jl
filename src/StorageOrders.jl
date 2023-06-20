6b9a4ff3d4e6c357462605f615891f6a00d028f9
Will Kimmerer <kimmerer@mit.edu>
2023-06-20 14:45:30 -0400

    bad storageorder defn

--- src/StorageOrders.jl
+++ src/StorageOrders.jl
@@ -7,11 +7,11 @@ struct RowMajor <: StorageOrder end #lexicographic ordering
 struct NoOrder <: StorageOrder end # something like a hash map sparse array.
 struct RuntimeOrder <: StorageOrder end # Order is known only at runtime, needs function barrier or manual redispatch.
 
-function storageorder(A::AbstractArray)
+function storageorder(A)
     order = comptime_storageorder(A)
     return order === RuntimeOrder() ? runtime_storageorder(A) : order
 end
 
 comptime_storageorder(::AbstractArray) = ColMajor()
-runtime_storageorder(::AbstractArray) = ColMajor()
+runtime_storageorder(::AbstractArray) = throw(ArgumentError("Runtime ordering is not defined for this type."))
 end
